dd550cf2fba3366a7de37d32be14bc39602b7915
bhavuksharma <bhavukshm@gmail.com>
2023-05-05 16:55:48 +0530

    fixed text-outflow in project unit

--- src/components/ProjectUnit.js
+++ src/components/ProjectUnit.js
@@ -7,7 +7,7 @@ function Project(props) {
     return (
         <div id="project">
             <a href={props.link}>
-                <h2>{props.title}<span className="year"> ({props.year})</span></h2>
+                <h2>{props.title} <br /><span className="year"> ({props.year})</span></h2>
                 <p>{props.description}</p>
             </a>
         </div>
